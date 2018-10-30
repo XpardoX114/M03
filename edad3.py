e1d260958ce53eb48fae8f6d52878ce3bd3053f9
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-10-30 09:15:19 +0100

    Update edad3.py

--- edad3.py
+++ edad3.py
@@ -1,5 +1,5 @@
 edad = int(input("Indique su edad: "))
 if ((edad>=17)and(edad<=23)):
-    print("Puedes entrar en la sesion de juego.")
+    print("Puedes entrar en la sesión de juego.")
 else:
-    print("Con", edad, "no puedes entrar en la sesion de juego.")
+    print("Con", edad, "no puedes entrar en la sesión de juego.")
