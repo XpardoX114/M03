7cd53b7f580bc7b47de075f2e672c1f291ebc75f
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-10-30 12:32:05 +0100

    Update ejercicio-edad-1.py

--- ejercicio-edad-1.py
+++ ejercicio-edad-1.py
@@ -1,1 +1,6 @@
 
+edad = int(input("Indica tu edad: "))
+if ((edad>=15)and(edad<=17)):
+    print("Puedes entrar en la sesión de jóvenes.")
+else:
+print("Con", edad , "no puedes entrar en la sesión de jóvenes.")
