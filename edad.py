0584c97019aa5d3c2919b65094fde37cb3ef3205
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-10-30 09:13:10 +0100

    Create edad.py

---
+++ edad.py
@@ -0,0 +1,5 @@
+edad = int(input("Indique su edad: "))
+if ((edad>=17)and(edad<=23)):
+    print("Puedes entrar en la sesion de juego.")
+else:
+    print("Con", edad, "no puedes entrar en la sesion de juego.")
