60c32395949f1039fa3355c9f558f230725f5da1
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-06 12:22:46 +0100

    Update Ganar 3.000 al mes.py

--- Ganar 3.000 al mes.py
+++ Ganar 3.000 al mes.py
@@ -1,9 +1,9 @@
 loteria=input("Te a tocado la loteria? ")
-if(loteria=="Si"):
+if(loteria=="Si")or(loteria=="si")or(loteria=="sI")or(loteria=="SI"):
     print("Puedes ganar 3.000 euros al mes")
 
 casar=input("Te casas con un(a) milloneti? ")
-edad=int(input("Que edad tiene? "))
+edad=int(input("Que ed""ad tiene? "))
 enfermedad=input("Tiene problemas de corazon? ")
 casada=input("Esta casado(a)? ")
 if((casar=="Si")and(edad>=80)and(enfermedad=="Si")and(casada=="No")):
