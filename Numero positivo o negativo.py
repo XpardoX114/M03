6282d09f5386983c55520a3ad7183e0229b76008
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-08 10:25:13 +0100

    Update Numero positivo o negativo.py

--- Numero positivo o negativo.py
+++ Numero positivo o negativo.py
@@ -1,23 +1,31 @@
-EJERCICIO:
-Hacer un codigo el cual te indique si el numero es positivo o negativo.
+#EJERCICIO:
+#Hacer un codigo el cual te indique si el numero es positivo o negativo.
+#JUEGO DE PUREBAS:
+  #  - Si el número es mas garande que 0
+  #  - Si el número es mas pequeño que 0
+  #  - Si el número es igual a 0
+  #  - Si el numero n es negativo
+  #  - Si el 0 es positivo, negativo o neutral
 
-JUEGO DE PUREBAS:
-    - Si el número es mas garande que 0
-    - Si el número es mas pequeño que 0
-    - Si el número es igual a 0
-    - Si el numero n es negativo
-    - Si el 0 es negativo
-
-EL 0 ES POSITIVO:
+#EL 0 ES POSITIVO:
 numero=int(input("Indique un número: "))
 if(numero<0):
     print("El número",numero,"es negativo")
 if(numero>=0):
     print("El número",numero,"es positivo")
 
-EL 0 ES NEGATIVO:
+#EL 0 ES NEGATIVO:
 numero=int(input("Indique un número: "))
 if(numero<=0):
     print("El número",numero,"es negativo")
 if(numero>0):
     print("El número",numero,"es positivo")
+    
+#EL 0 ES NEUTRAL:
+numero=int(input("Indique un número: "))
+if(numero<0):
+    print("El número",numero,"es negativo")
+if(numero>0):
+    print("El número",numero,"es positivo")
+if(numero==0):
+    print("El número",numero,"es neutral")
