1dae8e4a17c28bd281d566ad98b89f7159fdc94b
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-08 10:09:43 +0100

    Update Numero positivo o negativo.py

--- Numero positivo o negativo.py
+++ Numero positivo o negativo.py
@@ -4,7 +4,5 @@ Hacer un codigo el cual te indique si el numero es positivo o negativo.
 numero=int(input("Indique un número: "))
 if(numero<0):
     print("El número",numero,"es negativo")
-if(numero>0):
-    print("El número",numero,"es positivo")
-if(numero==0):
+if(numero>=0):
     print("El número",numero,"es positivo")
