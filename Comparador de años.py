22a47ee2cd8dba41e0f882547c4c618f88650a27
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-08 09:11:16 +0100

    Update Comparador de años.py

--- Comparador de años.py
+++ Comparador de años.py
@@ -1,1 +1,14 @@
+CAPRADOR DE AÑOS:
 
+anyo_actual=int(input("En que año estamos? "))
+anyo_cualquiera=int(input("Indique un año cualquiera: "))
+if(anyo_actual==anyo_cualquiera):
+    print("Es el mismo año")
+else:
+    if(anyo_actual<anyo_cualquiera):
+        solucion1=(anyo_actual-anyo_cualquiera)
+        print("Para llegar a año",anyo_cualquiera,"faltan",solucion1,"años")
+    else:
+        if(anyo_actual>anyo_cualquiera):
+            solucion2=(anyo_cualquiera-anyo_actual)
+            print("Para llegar a año",anyo_cualquiera,"faltan",solucion2,"años")
