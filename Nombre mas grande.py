d1e6d96f51b77ed987b8deae0b33220b06d5523c
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-15 10:31:14 +0100

    Create Nombre mas grande.py

---
+++ Nombre mas grande.py
@@ -0,0 +1,14 @@
+#Leer dos nombres i mostrar el mas grande:
+
+#JUEGO DE PRUEBAS:
+# Si los 2 numeros son iguales
+
+#CON 2 NUMEROS
+numero1=int(input("Indique un número: "))
+numero2=int(input("Indique un segundo número: "))
+if(numero1>numero2):
+    print("El número",numero1,"es el mas grande")
+else:
+    print("El número",numero2,"es el mas grande")
+    
+#CON 3 NUMEROS
