da6e2a2bc6ef059f492f974cc3b2549c9b3b7fb1
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-22 09:55:41 +0100

    Update Par o impar.py

--- Par o impar.py
+++ Par o impar.py
@@ -1,5 +1,5 @@
 numero1=int(input("Indique un número: "))
 if (numero1%2==0):
     print("El número", numero1 , "es par")
-if (numero1%2!=0):
+elif (numero1%2!=0):
     print("El número", numero1 , "es impar")
