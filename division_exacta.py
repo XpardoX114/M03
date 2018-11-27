e713afb261f5b760d3a733b45a03fa771bd37e50
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-27 13:03:20 +0100

    Create division_exacta.py

---
+++ division_exacta.py
@@ -0,0 +1,10 @@
+#Escriba un programa que pida dos números enteros y que calcule su división, escribiendo si la división es exacta o no.
+#DIVISOR DE NÚMEROS
+#Escriba el dividendo: 14
+#Escriba el divisor: 5
+#La división no es exacta. Cociente: 2 Resto: 4
+
+#DIVISOR DE NÚMEROS
+#Escriba el dividendo: 20
+#Escriba el divisor: 4
+#La división es exacta. Cociente: 5
