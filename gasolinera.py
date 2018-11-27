d9950c4f93c831b2e26c62ca33da2391086a71a4
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-27 13:02:16 +0100

    Update gasolinera.py

--- gasolinera.py
+++ gasolinera.py
@@ -4,3 +4,10 @@
 #-Diesel: Normal(1.7€), Fast&Furius(1.75€)
 #Luego peguntamos cuantos litros.
 #Le decimos el importe
+
+Codigo:
+gasolina=int(input("Super(1), Sin plomo(2), Diesel(3)"))
+if(gasolina=)
+1=int(input("Normal(1), Turbo(2)"))
+2=int(input("Normal(1), Con aditivos(2)"))
+3=int(input("Normal(1), Fast&Furius(2)"))
