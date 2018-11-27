dfa48f6b488cdca8a082b65c9f101770a6e2c433
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-27 14:01:34 +0100

    Update division_exacta.py

--- division_exacta.py
+++ division_exacta.py
@@ -11,8 +11,8 @@
 
 #CODIGO:
 
-dividendo=float(input("Indique el dividendo: "))
-divisor=float(input("Indique el divisor: "))
+dividendo=int(input("Indique el dividendo: "))
+divisor=int(input("Indique el divisor: "))
 if(dividendo<=0 or divisor<=0)or(dividendo<=0 and divisor<=0):
     print("El dividendo i/o el divisor es 0")
 else: 
