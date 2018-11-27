694b4fbbd84e7de0f18c9499fd69fc51cc13f3b2
AlexPardoGonzalez <44458468+XpardoX114@users.noreply.github.com>
2018-11-27 12:31:26 +0100

    Create gasolinera.py

---
+++ gasolinera.py
@@ -0,0 +1,6 @@
+#Preguntamos al usuario que tipo de gasolina quiere:
+#-Super: Normal(1.5€), Turbo(1.55€)
+#-Sin plomo: Normal(1.6€), Con aditivos(sabor naranja)(1.65€)
+#-Diesel: Normal(1.7€), Fast&Furius(1.75€)
+#Luego peguntamos cuantos litros.
+#Le decimos el importe
